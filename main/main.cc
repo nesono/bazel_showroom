9f0ac80e3411aff61be3a4d42ed5575ce1ff37ca
Jochen Issing <jochen.issing@aid-driving.eu>
2018-03-16 16:21:21 +0100

    use the dependency

--- main/main.cc
+++ main/main.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 
-//#include "pilib.h"
+#include "pilib.h"
 
 int main(int argc, char** argv)
 {
     std::cout << "Today: " << __DATE__ << std::endl;
     std::cout << "Time:  " << __TIME__ << std::endl;
     std::cout << "Hello Ma(i)n" << std::endl;
+    pi::printPi();
     return 0;
 }
