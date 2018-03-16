0ab5ebb57d8d354221c4c18663891eed9ad50f17
Jochen Issing <jochen.issing@aid-driving.eu>
2018-03-16 15:45:17 +0100

    add first library with legacy directory structure

---
+++ pilib/src/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace pi
+{
+namespace prvt 
+{
+float pi = 3.141592653589;
+}
+}
+
+#endif
+
---
+++ pilib/src/pilib.cc
@@ -0,0 +1,14 @@
+#include "pilib.h"
+#include "constants.h"
+
+#include <iostream>
+
+namespace pi
+{
+
+void printPi()
+{
+    std::cout << "Pi is: " <<  prvt::pi << std::endl;
+}
+
+}
