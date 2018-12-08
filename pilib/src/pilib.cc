2d17b932f90df87cf2dde646704159150a5fdf75
Jochen Issing <jochen.issing@aid-driving.eu>
2018-12-08 21:29:23 +0100

    Create a new revision affecting pilib

--- pilib/src/pilib.cc
+++ pilib/src/pilib.cc
@@ -10,11 +10,6 @@ namespace pi
 void printPi()
 {
     std::cout << "buildRevision   " << buildRevision() << std::endl;
-    std::cout << "buildStatus     " << buildStatus() << std::endl;
-    std::cout << "buildTimestamp  " << buildTimestamp() << std::endl;
-    std::cout << "buildEmbedLabel " << buildEmbedLabel() << std::endl;
-    std::cout << "buildBranch     " << buildBranch() << std::endl << std::endl;
-
     std::cout << "Pi is: " <<  prvt::pi << std::endl;
 }
 
